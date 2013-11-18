365f92e55ead86cb7a9c0af8b07861219a976e22
Salim Benchekroun <salimben60@gmail.com>
2013-11-17 23:03:39 -0800

    Updated the views

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,4 +2,9 @@ class Actor < ActiveRecord::Base
 
   has_and_belongs_to_many :movies
 
+
+def to_s
+  "#{self.name} (#{self.origin})"
+end
+
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,8 @@ class Movie < ActiveRecord::Base
 
   has_and_belongs_to_many :actors
 
-
+def to_s
+  "#{self.title} (#{self.year})"
+end
 
 end
